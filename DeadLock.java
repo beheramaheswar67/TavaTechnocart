497d0f0a9a9c67153dbcfb8292a7135f75e49c5b
beheramaheswar67 <beheramaheswar67@gmail.com>
2026-06-27 20:44:55 +0530

    DeadLock

---
+++ DeadLock.java
@@ -0,0 +1,50 @@
+public class DeadLock{
+
+            static final String s1=new String("Java");
+            static final String s2=new String("Technocard");
+
+            public static void main(String args[]){
+
+            Thread t1=new Thread(new Runnable() {
+            @Override
+            public void run(){
+            synchronized(s1){
+            System.out.println("Thread1 Locked "+s1+" Object");
+            try{
+
+               Thread.sleep(100);
+
+              }catch(InterruptedException ie){
+                  }
+            synchronized(s2){
+            System.out.println("Thread Locked "+s2+" Object");
+                  }
+               }
+           }
+        }
+        );
+
+          Thread t2=new Thread(new Runnable() {
+            @Override
+            public void run(){
+            synchronized(s1){
+            System.out.println("Thread1 Locked "+s1+" Object");
+            try{
+
+               Thread.sleep(100);
+
+              }catch(InterruptedException ie){
+                  }
+            synchronized(s2){
+            System.out.println("Thread Locked "+s2+" Object");
+                  }
+               }
+           }
+        }
+        );
+
+      t1.start();
+      t2.start();
+    }
+
+}
